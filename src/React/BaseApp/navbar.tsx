8cd2322b60f5c7dc4c7f356c7349e512a7e8da95
Harsh Kumar CHoudhary <harshkchoudhary2001@gmail.com>
2025-01-22 00:13:59 +0530

    Test Navbar

--- src/React/BaseApp/navbar.tsx
+++ src/React/BaseApp/navbar.tsx
@@ -1,8 +1,15 @@
-import { Stack } from "@mantine/core";
+import { Button, Stack } from "@mantine/core";
+import { IconMenu } from "@tabler/icons-react";
 import React from "react";
 
 const Navbar = () => {
-    return <Stack></Stack>;
+    return (
+        <Stack align="center" justify="center">
+            <Button w={224} color="#1e1b4b" radius={8} leftSection={<IconMenu></IconMenu>}>
+                Model Library
+            </Button>
+        </Stack>
+    );
 };
 
 export default Navbar;
